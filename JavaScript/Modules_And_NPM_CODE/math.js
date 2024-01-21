05925f7da804182236c50e43c0eedf28c004a94d
Eshaan Pandey <eshaanpandey.ep@gmail.com>
2024-01-21 15:30:21 +0530

    modules

---
+++ JavaScript/Modules_And_NPM_CODE/math.js
@@ -0,0 +1,22 @@
+const add = (x, y) => x + y;
+
+const PI = 3.14159;
+
+const square = x => x * x;
+
+exports.square = square;
+exports.PI = PI;
+
+// const math = {
+//     add: add,
+//     PI: PI,
+//     square: square
+// }
+// module.exports = math;
+
+// =========================
+// module.exports.add = add;
+// module.exports.PI = PI;
+// module.exports.square = square;
+
+
